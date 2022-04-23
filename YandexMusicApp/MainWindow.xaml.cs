2ee5df1e175e4bea4f235e1c62a3fddd5874492a
Denis Ignachev <denizik1@mail.ru>
2022-04-23 22:06:55 +0300

    Added HandyControls. Added main window frontend. Added icon resources.

--- YandexMusicApp/MainWindow.xaml.cs
+++ YandexMusicApp/MainWindow.xaml.cs
@@ -1,28 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using HandyControl.Tools.Extension;
 
 namespace YandexMusicApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadCoverFlow();
+        }
+
+        private void LoadCoverFlow()
+        {
+            
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Click");
         }
     }
 }
