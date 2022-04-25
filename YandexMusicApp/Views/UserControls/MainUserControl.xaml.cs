dc1992998e1d9bf95cf132520cdc826d870468f5
Denis Ignachev <denizik1@mail.ru>
2022-04-25 22:36:10 +0300

    Added content templates.

---
+++ YandexMusicApp/Views/UserControls/MainUserControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace YandexMusicApp.Views.UserControls;
+
+public partial class MainUserControl : UserControl
+{
+    public MainUserControl()
+    {
+        InitializeComponent();
+    }
+}
