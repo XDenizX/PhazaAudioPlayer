90653be5339433200cfdbd4e0d5e46d0c2a768ee
Denis Ignachev <denizik1@mail.ru>
2022-04-25 22:35:25 +0300

    Added content provider.

---
+++ YandexMusicApp/Infrastructure/Interfaces/IProviderOf.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YandexMusicApp.Infrastructure.Interfaces;
+
+public interface IProviderOf<T>
+{
+    void Register(T objectToRegister);
+    void Unregister(T objectToUnregister);
+
+    T Get(Type objectType);
+    TObject Get<TObject>() where TObject : T;
+}
