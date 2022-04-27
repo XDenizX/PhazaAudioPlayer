461eaa5806b827e1592b3d57fb0db0377a21376f
Ryavell <valeriy223@yandex.ru>
2022-04-28 02:57:32 +0300

    Decoupling AudioPlayer

--- Player/Players/NAudioPlayer.cs
+++ Player/Players/NAudioPlayer.cs
@@ -1,32 +1,14 @@
 ﻿using NAudio.Wave;
-using YandexMusicApp.AudioPlayer.Models;
-using YandexMusicApp.AudioPlayer.StreamProviders;
 
 namespace YandexMusicApp.AudioPlayer.Players
 {
     public class NAudioPlayer : IAudioPlayer
     {
-        public Track Current => Playlist.Tracks[_currentIndex];
-        private int _currentIndex;
-
-        private Playlist _playlist;
-        public Playlist Playlist
-        {
-            get => _playlist;
-            set
-            {
-                _currentIndex = 0;
-                _playlist = value;
-            }
-        }
         public bool IsPlaying => _waveOut.PlaybackState == PlaybackState.Playing;
+        public bool IsReadyToPlay => _waveStream != null;
 
-        public IAudioPlayer.RepeatMode Repeat { get; set; }
-        public bool IsShuffling { get; set; }
+        public event Action? StreamEnded;
 
-        public event Action<Track>? TrackEnded;
-        public event Action<Playlist>? PlaylistEnded;
-        public event Action<Playlist>? PlaylistRoundEnded;
         public float Volume
         {
             get => _waveOut.Volume;
@@ -53,86 +35,42 @@ namespace YandexMusicApp.AudioPlayer.Players
             }
         }
 
-        private readonly List<IStreamProvider> _streamProviders;
-
         private readonly WaveOutEvent _waveOut;
         private WaveStream? _waveStream;
 
-        private bool _fakeStop;
-        private bool _playlistEnded;
+        private bool _unhandledStop;
 
         public NAudioPlayer()
         {
-            _streamProviders = new List<IStreamProvider>
-            {
-                new LocalStorageStreamProvider()
-            };
-
             _waveOut = new WaveOutEvent();
             _waveOut.PlaybackStopped += OnPlaybackStopped;
         }
 
         private void OnPlaybackStopped(object? sender, EventArgs e)
         {
-            if (_fakeStop)
+            if (_unhandledStop)
             {
-                _fakeStop = false;
+                _unhandledStop = false;
                 return;
             }
 
-            TrackEnded?.Invoke(Current);
-
-            if (Repeat == IAudioPlayer.RepeatMode.Track)
-            {
-                if (_waveStream != null)
-                {
-                    Rewind();
-                    _waveOut.Play();
-                }
-            }
-            else
-            {
-                Next();
-                if (!(_playlistEnded && Repeat == IAudioPlayer.RepeatMode.None))
-                {
-                    Play();
-                }
-            }
+            StreamEnded?.Invoke();
         }
 
-        private Stream? GetTrackStream()
+        private void CallUnhandledStop()
         {
-            Stream? stream = null;
-
-            foreach (IStreamProvider streamProvider in _streamProviders)
-            {
-                stream = streamProvider.LoadStream(Current);
-            }
-
-            return stream;
-        }
-
-        private void FakeStop()
-        {
-            _fakeStop = true;
+            _unhandledStop = true;
             _waveOut.Stop();
         }
 
-        private void UpdateSteam()
+        public void SetStream(Stream stream)
         {
             if (_waveOut.PlaybackState != PlaybackState.Stopped)
             {
-                FakeStop();
+                CallUnhandledStop();
             }
             
             _waveStream?.Close();
-
-            Stream? stream = GetTrackStream();
-
-            if (stream == null)
-            {
-                throw new NullReferenceException("Can't get audio stream");
-            }
 
             _waveStream = new StreamMediaFoundationReader(stream);
             _waveOut.Init(_waveStream);
@@ -140,72 +78,12 @@ namespace YandexMusicApp.AudioPlayer.Players
 
         public void Play()
         {
-            if (_waveOut.PlaybackState != PlaybackState.Paused)
-            {
-                UpdateSteam();
-            }
-
-            _playlistEnded = false;
             _waveOut.Play();
         }
 
         public void Pause()
         {
             _waveOut.Pause();
-        }
-
-        public void Next()
-        {
-            if (IsShuffling)
-            {
-                _currentIndex = Shuffle();
-            }
-            else
-            {
-                if (_currentIndex < Playlist.Tracks.Count - 1)
-                {
-                    _currentIndex++;
-                }
-                else
-                {
-                    if (Repeat == IAudioPlayer.RepeatMode.Playlist)
-                    {
-                        _currentIndex = 0;
-                        PlaylistRoundEnded?.Invoke(Playlist);
-                    }
-
-                    _playlistEnded = true;
-                    PlaylistEnded?.Invoke(Playlist);
-                }
-            }
-        }
-
-        private void Rewind()
-        {
-            if (_waveStream != null)
-            {
-                _waveStream.Position = 0;
-            }
-        }
-
-        public void Previous()
-        {
-            if (Position < 0.03f)
-            {
-                if (_currentIndex > 0)
-                {
-                    _currentIndex--;
-                }
-            }
-            else
-            {
-                Rewind();
-            }
-        }
-
-        protected virtual int Shuffle()
-        {
-            return Random.Shared.Next(Playlist.Tracks.Count);
         }
     }
 }
