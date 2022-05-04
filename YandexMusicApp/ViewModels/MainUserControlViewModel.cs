18901526a21b14e400a482c9658440a4f73f02ca
Denis Ignachev <denizik1@mail.ru>
2022-05-05 02:00:36 +0300

    PlaylistViewModel replaced into other file.

--- YandexMusicApp/ViewModels/MainUserControlViewModel.cs
+++ YandexMusicApp/ViewModels/MainUserControlViewModel.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Reactive.Linq;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using DynamicData;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
@@ -13,31 +9,6 @@ namespace YandexMusicApp.ViewModels;
 
 public class MainUserControlViewModel : ReactiveObject
 {
-    // TODO: Replace in single class.
-    public class PlaylistViewModel : ReactiveObject
-    {
-        [Reactive] public string ImageUrl { get; init; }
-        [Reactive] public string Name { get; init; }
-        [Reactive] public string Artist { get; set; }
-        
-        public extern ImageSource ImageSource { [ObservableAsProperty] get; }
-
-        public PlaylistViewModel()
-        {
-            ImageUrl = string.Empty;
-            
-            this.WhenAnyValue(x => x.ImageUrl)
-                .Where(x => !string.IsNullOrEmpty(x))
-                .Select(GetImageSource)
-                .ToPropertyEx(this, x => x.ImageSource);
-        }
-
-        private ImageSource GetImageSource(string url)
-        {
-            return new BitmapImage(new Uri(url, UriKind.Absolute));
-        }
-    }
-    
     [Reactive] public ObservableCollection<PlaylistViewModel> Playlists { get; set; }
 
     public MainUserControlViewModel()
---
+++ YandexMusicApp/ViewModels/PlaylistViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace YandexMusicApp.ViewModels;
+
+public class PlaylistViewModel : ReactiveObject
+{
+    [Reactive] public string ImageUrl { get; init; }
+    [Reactive] public string Name { get; init; }
+    [Reactive] public string Artist { get; set; }
+        
+    public extern ImageSource ImageSource { [ObservableAsProperty] get; }
+
+    public PlaylistViewModel()
+    {
+        ImageUrl = string.Empty;
+            
+        this.WhenAnyValue(x => x.ImageUrl)
+            .Where(x => !string.IsNullOrEmpty(x))
+            .Select(GetImageSource)
+            .ToPropertyEx(this, x => x.ImageSource);
+    }
+
+    private ImageSource GetImageSource(string url)
+    {
+        return new BitmapImage(new Uri(url, UriKind.Absolute));
+    }
+}
