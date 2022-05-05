13718a4a334a400656663a1c2cb1be3dc942d9e7
Denis Ignachev <denizik1@mail.ru>
2022-05-06 01:03:00 +0300

    Added double to time converter.

---
+++ YandexMusicApp/Converters/DoubleToTimeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace YandexMusicApp.Converters;
+
+public class DoubleToTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var seconds = (double) value;
+
+        return TimeSpan.FromSeconds(Math.Round(seconds)).ToString(@"mm\:ss");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
