ff0702ef0b82c2149440de03d847d7dc03aed7bb
XDenizX <denizik1@mail.ru>
2022-07-25 16:11:42 +0300

    Cleared test code. Removed user settings.

--- PhazaAudioPlayer/Views/MainWindow.xaml.cs
+++ PhazaAudioPlayer/Views/MainWindow.xaml.cs
@@ -20,23 +20,7 @@ namespace PhazaAudioPlayer.Views
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("Click");
 
-            StreamFactory streamFactory = new StreamFactory();
-            streamFactory.RegisterProviders(new List<IStreamProvider>
-        {
-            new MemoryStreamProvider(),
-            new LocalStorageStreamProvider(),
-            new HttpStreamProvider(),
-        });
-
-            NAudioPlayer nAudioPlayer = new NAudioPlayer();
-            PlayerController player = new PlayerController(nAudioPlayer, streamFactory);
-
-            TestPlaylistProvider playlistProvider = new TestPlaylistProvider();
-
-            player.Playlist = playlistProvider.GetPlaylist("_");
-            player.StartPlayback();
         }
     }
 }
