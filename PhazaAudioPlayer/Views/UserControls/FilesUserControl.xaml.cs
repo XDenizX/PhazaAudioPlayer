5068873ba8429c38d49a60b6a8f2a518239aafbd
XDenizX <denizik1@mail.ru>
2022-07-28 19:56:32 +0300

    Implemented IViewFor interface in files user control.

--- PhazaAudioPlayer/Views/UserControls/FilesUserControl.xaml.cs
+++ PhazaAudioPlayer/Views/UserControls/FilesUserControl.xaml.cs
@@ -1,9 +1,24 @@
-﻿using System.Windows.Controls;
+﻿using PhazaAudioPlayer.ViewModels;
+using ReactiveUI;
 
 namespace PhazaAudioPlayer.Views.UserControls;
 
-public partial class FilesUserControl : UserControl
+public partial class FilesUserControl : UserControl, IViewFor<FilesUserControlViewModel>
 {
+    #region ViewModel
+    public FilesUserControlViewModel? ViewModel
+    { 
+        get => DataContext as FilesUserControlViewModel;
+        set => DataContext = value;
+    }
+
+    object? IViewFor.ViewModel 
+    { 
+        get => ViewModel;
+        set => ViewModel = (FilesUserControlViewModel) value;
+    }
+    #endregion
+
     public FilesUserControl()
     {
         InitializeComponent();
