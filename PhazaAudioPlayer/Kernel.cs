f744a09f46565a4c4e9c5d7ff82f2f6e7edbd9e4
XDenizX <denizik1@mail.ru>
2022-08-10 18:48:16 +0300

    Integrated the Ninject package.

---
+++ PhazaAudioPlayer/Kernel.cs
@@ -0,0 +1,20 @@
+﻿using Ninject;
+using PhazaAudioPlayer.Infrastructure.Modules;
+
+namespace PhazaAudioPlayer
+{
+    public static class Kernel
+    {
+        private static IKernel _kernel;
+
+        public static T Get<T>()
+        {
+            if (_kernel == null)
+            {
+                _kernel = new StandardKernel(new BindModule());
+            }
+
+            return _kernel.Get<T>();
+        }
+    }
+}
