6a1ef5da6e1b3df0f062597bf687a492490fb5ac
XDenizX <denizik1@mail.ru>
2022-08-11 19:13:30 +0300

    Specified a argument type in switch content method.

--- PhazaAudioPlayer/ViewModels/SideMenuViewModel.cs
+++ PhazaAudioPlayer/ViewModels/SideMenuViewModel.cs
@@ -3,6 +3,7 @@ using PhazaAudioPlayer.Infrastructure.Providers;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
+using System.Net.Mime;
 using System.Reactive;
 using System.Windows.Controls;
 
@@ -10,28 +11,23 @@ namespace PhazaAudioPlayer.ViewModels
 {
     public class SideMenuViewModel : ReactiveObject
     {
-        [Reactive] public ReactiveCommand<object, Unit> SwitchContentCommand { get; set; }
+        [Reactive] public ReactiveCommand<Type, Unit> SwitchContentCommand { get; set; }
 
         private readonly IProviderOf<UserControl> _contentProvider = new ContentProvider();
 
         public UserControl CurrentContent { get; private set; }
 
         public delegate void ContentSwitchedHandler(UserControl content);
-        public event ContentSwitchedHandler ContentSwitched; 
+        public event ContentSwitchedHandler ContentSwitched;
 
         public SideMenuViewModel()
         {
             SwitchContentCommand = ReactiveCommand
-                .Create<object>(content => SwitchContent(content));
+                .Create<Type>(contentType => SwitchContent(contentType));
         }
 
-        private void SwitchContent(object args)
+        private void SwitchContent(Type contentType)
         {
-            if (args is not Type contentType)
-            {
-                return;
-            }
-
             if (contentType.IsAssignableTo(typeof(UserControl)))
             {
                 CurrentContent = _contentProvider.Get(contentType);
