5846634828f04b12466b1e5b7dac51ef56ae6180
XDenizX <denizik1@mail.ru>
2022-08-11 18:19:59 +0300

    Made the audio player panel as user control.

--- PhazaAudioPlayer/Infrastructure/Modules/BindModule.cs
+++ PhazaAudioPlayer/Infrastructure/Modules/BindModule.cs
@@ -12,6 +12,7 @@ namespace PhazaAudioPlayer.Infrastructure.Modules
             Bind<IAudioPlayer>().To<NAudioPlayer>().InTransientScope();
             Bind<PlaybackService>().ToSelf().InSingletonScope();
             Bind<SideMenuViewModel>().ToSelf().InSingletonScope();
+            Bind<PlayerPanelViewModel>().ToSelf().InSingletonScope();
         }
     }
 }
