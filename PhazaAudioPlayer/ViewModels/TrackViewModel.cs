6cde874cbdc593e0181f5cf3ea1fd264cf848abf
XDenizX <denizik1@mail.ru>
2022-08-12 19:51:44 +0300

    Extended track view model.

--- PhazaAudioPlayer/ViewModels/TrackViewModel.cs
+++ PhazaAudioPlayer/ViewModels/TrackViewModel.cs
@@ -1,9 +1,26 @@
-﻿using ReactiveUI.Fody.Helpers;
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace PhazaAudioPlayer.ViewModels;
 
-public class TrackViewModel
+public class TrackViewModel : ReactiveObject
 {
-    [Reactive] public string Path { get; set; }
+    [Reactive] public Uri Path { get; set; }
     [Reactive] public string Name { get; set; }
+    [Reactive] public string Artist { get; set; }
+    [Reactive] public bool IsPlaying { get; set; }
+    [Reactive] public bool IsLiked { get; set; }
+    [Reactive] public Uri ImageUri { get; set; }
+    [Reactive] public double Duration { get; set; } = 100;
+    [Reactive] public ImageSource Image { get; private set; }
+
+    public TrackViewModel()
+    {
+        var imageUriObserver = this.WhenAnyValue(x => x.ImageUri)
+            .WhereNotNull()
+            .Subscribe(uri => Image = new BitmapImage(uri));
+    }
 }
